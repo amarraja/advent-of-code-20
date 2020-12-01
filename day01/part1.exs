eedb47e0140e5c648036a620162ad2ca81a18516
Amar Raja <amaryraja@gmail.com>
2020-12-01 10:49:50 +0000

    Part 2

--- day01/part1.exs
+++ day01/part1.exs
@@ -8,6 +8,16 @@ defmodule Day01 do
     [{x, y} | _] = for x <- input, y <- input, x + y == 2020, do: {x, y}
     x * y
   end
+
+  def part2(input) do
+    input =
+      input
+      |> String.split()
+      |> Enum.map(&String.to_integer/1)
+
+    [{x, y, z} | _] = for x <- input, y <- input, z <- input, x + y + z == 2020, do: {x, y, z}
+    x * y * z
+  end
 end
 
 ExUnit.start()
@@ -26,11 +36,18 @@ defmodule Test do
     """
 
     assert Day01.part1(input) == 514_579
+    assert Day01.part2(input) == 241_861_950
   end
 
   test "part1" do
     File.read!("input1.txt")
     |> Day01.part1()
     |> IO.inspect(label: "part1")
+  end
+
+  test "part2" do
+    File.read!("input1.txt")
+    |> Day01.part2()
+    |> IO.inspect(label: "part2")
   end
 end
