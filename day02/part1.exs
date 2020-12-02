d981cfda443abb08ad6eeedba761e09c5edbc135
Amar Raja <amaryraja@gmail.com>
2020-12-02 11:31:54 +0000

    part 2

--- day02/part1.exs
+++ day02/part1.exs
@@ -20,6 +20,27 @@ defmodule Day02 do
 
     valid
   end
+
+  def part2(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.count(&is_valid_2/1)
+  end
+
+  def is_valid_2(line) do
+    [ranges, letter, password] = String.split(line)
+
+    [a, b] = ranges |> String.split("-") |> Enum.map(&String.to_integer/1)
+    letter = String.replace(letter, ":", "")
+
+    password = String.codepoints(password)
+    a = Enum.at(password, a - 1) == letter
+    b = Enum.at(password, b - 1) == letter
+
+    # Can't work out boolean XOR for some reason, quick alternative
+    (a || b) && a != b
+  end
 end
 
 ExUnit.start()
@@ -35,11 +56,18 @@ defmodule Test do
     """
 
     assert Day02.part1(input) == 2
+    assert Day02.part2(input) == 1
   end
 
   test "part1" do
     File.read!("input1.txt")
     |> Day02.part1()
     |> IO.inspect(label: "part1")
+  end
+
+  test "part2" do
+    File.read!("input1.txt")
+    |> Day02.part2()
+    |> IO.inspect(label: "part2")
   end
 end
